e956b65a73ca33f6e7c585b482a5e84e69772421
CuongNghiem <quangcuongnghiem@gmail.com>
2021-09-10 15:51:45 +0700

    Adding Index controller and Index page. closes #7.

---
+++ pet-clinic-web/src/main/java/com/cuongnghiem/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.cuongnghiem.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by cuongnghiem on 10/09/2021
+ **/
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
