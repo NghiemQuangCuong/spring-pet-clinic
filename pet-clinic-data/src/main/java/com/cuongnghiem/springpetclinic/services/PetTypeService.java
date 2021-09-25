49bfc1bedc259e8416570d93e939544db56fb8ce
CuongNghiem <quangcuongnghiem@gmail.com>
2021-09-25 08:40:30 +0700

    add PetType Map Service Impl. closes #29.

---
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.cuongnghiem.springpetclinic.services;
+
+import com.cuongnghiem.springpetclinic.model.PetType;
+
+/**
+ * Created by cuongnghiem on 25/09/2021
+ **/
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,11 @@
+package com.cuongnghiem.springpetclinic.services.map;
+
+import com.cuongnghiem.springpetclinic.model.PetType;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by cuongnghiem on 25/09/2021
+ **/
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> {
+}
