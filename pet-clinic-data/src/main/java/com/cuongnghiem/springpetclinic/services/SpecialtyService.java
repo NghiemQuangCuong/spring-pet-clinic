571b060ce1e2aa59ef115f00753b7abdc6a9c878
CuongNghiem <quangcuongnghiem@gmail.com>
2021-09-25 10:31:01 +0700

    Add Specialty Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/services/SpecialtyService.java
@@ -0,0 +1,10 @@
+package com.cuongnghiem.springpetclinic.services;
+
+import com.cuongnghiem.springpetclinic.model.Specialty;
+
+/**
+ * Created by cuongnghiem on 25/09/2021
+ **/
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,12 @@
+package com.cuongnghiem.springpetclinic.services.map;
+
+import com.cuongnghiem.springpetclinic.model.Specialty;
+import com.cuongnghiem.springpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by cuongnghiem on 25/09/2021
+ **/
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+}
