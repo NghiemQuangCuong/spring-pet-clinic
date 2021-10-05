4724ab4fbd695b9a1b6ffecc3aeec072db5c7ccf
CuongNghiem <quangcuongnghiem@gmail.com>
2021-10-05 13:43:02 +0700

    Added JPA Mappings Annotation for Owner. Close #43.

--- pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/model/Owner.java
@@ -1,12 +1,23 @@
 package com.cuongnghiem.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
