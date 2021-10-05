b5756f9ef599031b8929b092c6ef4cd652121c2b
CuongNghiem <quangcuongnghiem@gmail.com>
2021-10-05 11:32:12 +0700

    Implement Base Entity with JPA. Closes #35.

--- pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/cuongnghiem/springpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.cuongnghiem.springpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by cuongnghiem on 10/09/2021
  **/
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
