9ce205f1b0ac38245c93e8493491d51440667738
CuongNghiem <quangcuongnghiem@gmail.com>
2021-10-09 19:46:25 +0700

    Add Vet Map Service CRUD test. Add failsafe maven plugin for project.

--- pet-clinic-data/src/test/java/com/cuongnghiem/springpetclinic/services/map/OwnerServiceMapTest.java
+++
@@ -1,6 +0,0 @@
-package com.cuongnghiem.springpetclinic.services.map;
-
-class OwnerServiceMapTest {
-
-
-}
---
+++ pet-clinic-data/src/test/java/com/cuongnghiem/springpetclinic/services/map/VetServiceMapTest.java
@@ -0,0 +1,59 @@
+package com.cuongnghiem.springpetclinic.services.map;
+
+import com.cuongnghiem.springpetclinic.model.Specialty;
+import com.cuongnghiem.springpetclinic.model.Vet;
+import com.cuongnghiem.springpetclinic.services.SpecialtyService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class VetServiceMapTest {
+
+    private VetServiceMap vetServiceMap;
+
+    private Vet vet;
+
+    @Mock
+    SpecialtyService specialtyService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        vetServiceMap = new VetServiceMap(specialtyService);
+        vet = new Vet();
+    }
+
+    @Test
+    void save() {
+        Set<Specialty> specialties = new HashSet<>();
+        Specialty specialty0 = new Specialty();
+        specialties.add(specialty0);
+        Specialty specialty1 = new Specialty();
+        specialty1.setId(5L);
+        specialty1.setDescription("Specialty 1");
+        specialties.add(specialty1);
+        vet.setSpecialties(specialties);
+
+        ArgumentCaptor<Specialty> specialtyArgumentCaptor
+                = ArgumentCaptor.forClass(Specialty.class);
+
+        // when
+        Vet vetSaveReturn = vetServiceMap.save(vet);
+
+        assertNotNull(vet.getId());
+        assertEquals(vet, vetSaveReturn);
+        verify(specialtyService, times(1))
+                .save(specialtyArgumentCaptor.capture());
+        assertEquals(specialty0, specialtyArgumentCaptor.getValue());
+    }
+}
