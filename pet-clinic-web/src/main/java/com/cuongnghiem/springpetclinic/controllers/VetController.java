8a8f6c70dbde89be7a69551afb9e7a07bffb8ef8
CuongNghiem <quangcuongnghiem@gmail.com>
2021-10-20 16:49:56 +0700

    Vets List JSON Endpoint. Closes #60.

--- pet-clinic-web/src/main/java/com/cuongnghiem/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/cuongnghiem/springpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.cuongnghiem.springpetclinic.controllers;
 
+import com.cuongnghiem.springpetclinic.model.Vet;
 import com.cuongnghiem.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by cuongnghiem on 11/09/2021
@@ -23,5 +28,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
